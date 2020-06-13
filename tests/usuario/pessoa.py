35f2727c7b135413595996a892d040c439e3c96e
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-06-13 15:05:43 -0300

    refatorando testes

--- tests/usuario/pessoa.py
+++ tests/usuario/pessoa.py
@@ -4,14 +4,13 @@ class Pessoa():
         self.segundo_nome = segundo_nome
         self.altura = altura
         self.peso = peso
-    
+
     def full_name(self):
         return '%s %s' % (self.primeiro_nome, self.segundo_nome)
-    
+
     def imc(self):
         if isinstance(self.peso, float) and isinstance(self.altura, float):
             imc = self.peso / (float(self.altura) * float(self.altura))
             return imc
         else:
             return None
-        
