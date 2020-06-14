b22b86aa9015ad28f7e04ba06bbe6a1ec77352b7
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-06-14 00:13:28 -0300

    mais comentarios e mais um exemplo

--- directory.py
+++ directory.py
@@ -1,25 +1,31 @@
-#import os
+import shutil
+import os
 
-#print(os.getcwd())
+# junta as string para formar um caminho de um diretorio
+print(os.path.join('home', 'luiz'))
 
-#os.chdir('/home')
-#print(os.getcwd())
+# retorna o caminho do diretorio atual
+print(os.getcwd())
 
-#os.mkdir('new_dir')
-#print(os.listdir())
+# cria um novo diretorio
+os.mkdir('new_dir')
+# lista todos os diretorios e arquivos neste diretorio e retorna uma lista
+print(os.listdir())
 
-#os.rename('new_dir', 'r_new_dir')
-#print(os.listdir())
+# renomeia um diretorio
+os.rename('new_dir', 'r_new_dir')
+print(os.listdir())
 
-#os.remove('text.txt') # remove file
-#print(os.listdir())
+os.remove('test.txt')  # remove um arquivo
+print(os.listdir())
 
-#os.rmdir('r_new_dir') # remove directory
-#print(os.listdir())
+os.rmdir('r_new_dir')  # remove um diretorio
+print(os.listdir())
 
+# troca de diretorio
+os.chdir('/home')
+print(os.getcwd())
 
-'''remove a non-empty directory'''
-#import shutil
-
-#shutil.rmtree('example')
-#print(os.listdir())
+# remove um diretorio recursivamente
+shutil.rmtree('example')
+print(os.listdir())
