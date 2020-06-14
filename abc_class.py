81764702721b807e7037009421c4cb194ad14f81
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-06-13 21:15:15 -0300

    refatorando abc

--- abc_class.py
+++ abc_class.py
@@ -1,13 +1,29 @@
 from abc import ABCMeta, abstractmethod
 
+
 class Veiculo(metaclass=ABCMeta):
-    
     @abstractmethod
     def ligar(self):
         pass
-    
+
     @abstractmethod
     def desligar(self):
         pass
 
-#v = Veiculo() # error
+
+class Carro(Veiculo):
+    def ligar(self):
+        print('ligando')
+
+    def desligar(self):
+        print('desligando')
+
+
+try:
+    v = Veiculo()  # error
+except TypeError as e:
+    print(e)
+
+c = Carro()
+c.ligar()
+c.desligar()
