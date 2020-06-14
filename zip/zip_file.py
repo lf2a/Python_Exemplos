9bf16d7f1a6b1392434059b1b2182d48519e732e
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-06-13 22:16:50 -0300

    colocando espaços, melhorando os comentarios e trocando os arquivos de exemplo

--- zip/zip_file.py
+++ zip/zip_file.py
@@ -4,17 +4,17 @@ import datetime
 
 
 def create():
-    '''criar arquivo zip com os arquivos 'data.csv' e 'lorem.txt'
+    '''criar arquivo zip com os arquivos 'test/index.html' e 'test/index.html'
     '''
     with zipfile.ZipFile('arquivo.zip', 'w') as z:
-        z.write('data.csv')
-        z.write('lorem.txt')
+        z.write('test/index.html')
+        z.write('test/style.css')
 
 
 def create_dir():
     with zipfile.ZipFile('arquivo2.zip', 'w') as z:
-        # itera sobre todos os arquivos se diretorios
-        for folderName, _, filenames in os.walk('stock_price'):
+        # itera sobre todos os arquivos e diretorios
+        for folderName, _, filenames in os.walk('../tests'):
             for filename in filenames:
                 filePath = os.path.join(folderName, filename)
                 z.write(filePath)
@@ -22,26 +22,25 @@ def create_dir():
 
 if __name__ == '__main__':
     zf = zipfile.ZipFile('arquivo.zip', 'r')
-    print(zf.namelist())
+    print(zf.namelist())  # lista os arquivos detro de 'arquivo.zip'
+
     for filename in zf.namelist():
         try:
             data = zf.read(filename)
-            print(dir(zf))
         except KeyError:
-            print ('ERROR: Did not find %s in zip file' % filename)
+            print('ERROR: Did not find %s in zip file' % filename)
         else:
-            print(filename, ':')
-            print(repr(data))
-        
+            print(f'{filename}:\t{data}') # nome do arquivo: seu conteudo
+    print('')
+
     for info in zf.infolist():
-        print(info.filename)
+        print(f'\n{info.filename} -> info')
         print('\tComment:\t', info.comment)
         print('\tModified:\t', datetime.datetime(*info.date_time))
         print('\tSystem:\t\t', info.create_system, '(0 = Windows, 3 = Unix)')
         print('\tZIP version:\t', info.create_version)
         print('\tCompressed:\t', info.compress_size, 'bytes')
         print('\tUncompressed:\t', info.file_size, 'bytes')
-    
-    # create()
+
+    create()
     create_dir()
-    
