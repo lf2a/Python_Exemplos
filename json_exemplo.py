b2d330b5fa0a53c44c04695a56073eb82d9858bf
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-06-15 23:06:21 -0300

    colocando comentarios para explicar o codigo

--- json_exemplo.py
+++ json_exemplo.py
@@ -15,8 +15,12 @@ data = [
     }
 ]
 
+# converte um dicionário em uma string, ordenando as chaves (sort_keys=True) e identando com 4 espaços (indent=4)
 json_str = json.dumps(data, sort_keys=True, indent=4)
+print(type(json_str))
 print(json_str)
 
+# transforma uma string em um dict, neste caso, em uma lista de dicionários
 json_dict = json.loads(json_str)
+print(type(json_dict))
 print(json_dict)
