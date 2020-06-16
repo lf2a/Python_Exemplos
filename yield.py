58757ff8c4b9f911e7dd8c0a9103546b90dbcca7
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-06-15 23:49:57 -0300

    colocando comentarios, mundado nomes de variaveis e colocando uma expressão de exemplo

--- yield.py
+++ yield.py
@@ -1,12 +1,30 @@
-def even():
-    for i in range(100):
+# yield - retorna um generator
+
+"""
+    Na primeira vez que a função for executada, ela vai rodar do começo e parar até tocar no primeiro yield.
+
+    Após encontrar o primeiro yield, ela vai continuar do ponto que foi parado até tocar o próximo yield.
+
+    Quando não for encontrado um yield, a exceção StopIteration é lançada.
+"""
+
+
+def pares():
+    for i in range(10):
         if i % 2 == 0:
             yield '%d' % i
         else:
-            yield 'odd'
+            yield 'impar'
 
 
-list_ = even()
+lista = pares()
 
-for index, value in enumerate(list_):
-    print('list_[%s] = %s' % (index, value))
+# outra forma de acessar um generator, usando next()
+print(next(lista))
+print(next(lista))
+
+# enumerate(iterable) - recebe um iterable e retora dois valores, o primeiro é o indice e o sundo é o valor
+# referente a aquele indice
+for index, value in enumerate(lista):
+    print('lista[%s] = %s' % (index, value))
+
